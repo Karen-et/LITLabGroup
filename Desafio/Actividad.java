6f2f31407c243cfe8c74241479fc7802499e2b4a
Karen <66844562+Karen-et@users.noreply.github.com>
2021-04-08 13:12:06 -0300

    Update Actividad.java

--- Desafio/Actividad.java
+++ Desafio/Actividad.java
@@ -1,39 +1,44 @@
 package Desafio;
+
 public class Actividad {
 
 	public static void main(String[] args) {
 		
+		Estudiante[] estudiante = new Estudiante[12];
+		
 		//Parte 1
 				int[] notasE1 = {8,7,10};
-				Estudiante estudiante1 = new Estudiante(1, "Paula", notasE1);
+				estudiante[0] = new Estudiante(1, "Paula", notasE1);
 				
 				int[] notasE2 = {4,1,2};
-				Estudiante estudiante2 = new Estudiante(2, "Anthony", notasE2);
+				estudiante[1] = new Estudiante(2, "Anthony", notasE2);
 				
 				int[] notasE3 = {3,4,5};
-				Estudiante estudiante3 = new Estudiante(3, "Mauro", notasE3);
+				estudiante[2] = new Estudiante(3, "Mauro", notasE3);
 				
 				int[] notasE4 = {10,10,9};
-				Estudiante estudiante4 = new Estudiante(4, "Karen", notasE4);
+				estudiante[3] = new Estudiante(4, "Karen", notasE4);
 				
 				int[] notasE5 = {8,8,8};
-				Estudiante estudiante5 = new Estudiante(5, "Michael", notasE5);
+				estudiante[4] = new Estudiante(5, "Michael", notasE5);
 				
 				int[] notasE6 = {8,7,10};
-				Estudiante estudiante6 = new Estudiante(6, "José", 8,7,10);
+				estudiante[5] = new Estudiante(6, "José", notasE6);
 				
 				int[] notasE7 = {6,6,6};
-				Estudiante estudiante7 = new Estudiante(7, "Eliana", notasE7);
+				estudiante[6] = new Estudiante(7, "Eliana", notasE7);
 				
 				int[] notasE8 = {10,7,10};
-				Estudiante estudiante8 = new Estudiante(8, "Evelyn", notasE8);
+				estudiante[7] = new Estudiante(8, "Evelyn", notasE8);
 				
 				int[] notasE9 = {8,7,9};
-				Estudiante estudiante9 = new Estudiante(9, "Alejandra", notasE9);
+				estudiante[8] = new Estudiante(9, "Alejandra", notasE9);
 				
 				int[] notasE10 = {4,7,10};
-				Estudiante estudiante10 = new Estudiante(10, "Pepito", notasE10);
+				estudiante[9] = new Estudiante(10, "Pepito", notasE10);
+				
+		
 
 	}
-
+	
 }
