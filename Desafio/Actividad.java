1910730a33fc3b6272e652ccd4b0750593cb6cee
Karen <66844562+Karen-et@users.noreply.github.com>
2021-04-08 11:54:59 -0300

    Create Actividad.java

---
+++ Desafio/Actividad.java
@@ -0,0 +1,39 @@
+package Desafio;
+public class Actividad {
+
+	public static void main(String[] args) {
+		
+		//Parte 1
+				int[] notasE1 = {8,7,10};
+				Estudiante estudiante1 = new Estudiante(1, "Paula", notasE1);
+				
+				int[] notasE2 = {4,1,2};
+				Estudiante estudiante2 = new Estudiante(2, "Anthony", notasE2);
+				
+				int[] notasE3 = {3,4,5};
+				Estudiante estudiante3 = new Estudiante(3, "Mauro", notasE3);
+				
+				int[] notasE4 = {10,10,9};
+				Estudiante estudiante4 = new Estudiante(4, "Karen", notasE4);
+				
+				int[] notasE5 = {8,8,8};
+				Estudiante estudiante5 = new Estudiante(5, "Michael", notasE5);
+				
+				int[] notasE6 = {8,7,10};
+				Estudiante estudiante6 = new Estudiante(6, "José", 8,7,10);
+				
+				int[] notasE7 = {6,6,6};
+				Estudiante estudiante7 = new Estudiante(7, "Eliana", notasE7);
+				
+				int[] notasE8 = {10,7,10};
+				Estudiante estudiante8 = new Estudiante(8, "Evelyn", notasE8);
+				
+				int[] notasE9 = {8,7,9};
+				Estudiante estudiante9 = new Estudiante(9, "Alejandra", notasE9);
+				
+				int[] notasE10 = {4,7,10};
+				Estudiante estudiante10 = new Estudiante(10, "Pepito", notasE10);
+
+	}
+
+}
