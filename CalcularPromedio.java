88bb1b51ec0df49d096f63284479cbac45c990d6
AnthonyMR7 <anthony.martinez@estudiantes.utec.edu.uy>
2021-04-08 10:35:50 -0300

    Create CalcularPromedio.java

---
+++ CalcularPromedio.java
@@ -0,0 +1,44 @@
+package semana3;
+
+public class CalcularPromedio {
+	
+		public static void main(String[] args) {
+
+			int num1 = Integer.valueOf(args[0]);
+			int num2 = Integer.valueOf(args[1]);
+			int num3 = Integer.valueOf(args[2]);
+			int promedio= (num1 + num2 + num3) / 3;
+			int suma;
+			
+			if(promedio<10) {
+				System.out.println(promedio);
+			} 
+			else if (promedio>10) {
+					suma=promedio + num1 + num2 + num3;
+					System.out.println(suma);
+				  }
+		
+
+		
+				
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			// de aqu� para abajo, colocar la implementacion pedida
+
+
+		}
+
+	}
